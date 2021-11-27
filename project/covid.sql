44066380380d9b89e48a4f68195ec4fee69539da
anees <aneesurrehman423@gmail.com>
2021-11-27 08:01:22 +0530

    first commit

---
+++ project/covid.sql
@@ -0,0 +1,260 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 26, 2021 at 03:47 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `covid`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookingpatient`
+--
+
+CREATE TABLE `bookingpatient` (
+  `id` int(11) NOT NULL,
+  `srfid` varchar(50) NOT NULL,
+  `bedtype` varchar(50) NOT NULL,
+  `hcode` varchar(50) NOT NULL,
+  `spo2` int(11) NOT NULL,
+  `pname` varchar(50) NOT NULL,
+  `pphone` varchar(12) NOT NULL,
+  `paddress` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `bookingpatient`
+--
+
+INSERT INTO `bookingpatient` (`id`, `srfid`, `bedtype`, `hcode`, `spo2`, `pname`, `pphone`, `paddress`) VALUES
+(3, 'KA20210011', 'ICUBed', 'MAT123', 85, 'ARK', '9986786453', 'BANGALORE'),
+(4, 'KA20210022', 'ICUBed', 'BBH01', 92, 'kartik', '8088131784', 'banaglore ');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hospitaldata`
+--
+
+CREATE TABLE `hospitaldata` (
+  `id` int(11) NOT NULL,
+  `hcode` varchar(200) NOT NULL,
+  `hname` varchar(200) NOT NULL,
+  `normalbed` int(11) NOT NULL,
+  `hicubed` int(11) NOT NULL,
+  `icubed` int(11) NOT NULL,
+  `vbed` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `hospitaldata`
+--
+
+INSERT INTO `hospitaldata` (`id`, `hcode`, `hname`, `normalbed`, `hicubed`, `icubed`, `vbed`) VALUES
+(3, 'MAT123', 'Matha Hospital', 40, 4, 4, 1);
+
+--
+-- Triggers `hospitaldata`
+--
+DELIMITER $$
+CREATE TRIGGER `Insert` AFTER INSERT ON `hospitaldata` FOR EACH ROW INSERT INTO trig VALUES(null,NEW.hcode,NEW.normalbed,NEW.hicubed,NEW.icubed,NEW.vbed,' INSERTED',NOW())
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `Update` AFTER UPDATE ON `hospitaldata` FOR EACH ROW INSERT INTO trig VALUES(null,NEW.hcode,NEW.normalbed,NEW.hicubed,NEW.icubed,NEW.vbed,' UPDATED',NOW())
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `delet` BEFORE DELETE ON `hospitaldata` FOR EACH ROW INSERT INTO trig VALUES(null,OLD.hcode,OLD.normalbed,OLD.hicubed,OLD.icubed,OLD.vbed,' DELETED',NOW())
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hospitaluser`
+--
+
+CREATE TABLE `hospitaluser` (
+  `id` int(11) NOT NULL,
+  `hcode` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `hospitaluser`
+--
+
+INSERT INTO `hospitaluser` (`id`, `hcode`, `email`, `password`) VALUES
+(7, 'BBH01', 'aneesrehman95567@gmail.com', 'pbkdf2:sha256:260000$im6PllE9qrd0asQY$3e62fcb9697d2b048acd83cb3658bac8ae5edb5ff58086699d134fb0ed41d788'),
+(9, 'MAT123', 'arkprocoder@gmail.com', 'pbkdf2:sha256:260000$CPDXGkSY1zXsarEA$fdbec84d1b2c32e521c75f51bb917daaa4f7a53e567e4478d23ba944c53b1177');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `test`
+--
+
+CREATE TABLE `test` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `test`
+--
+
+INSERT INTO `test` (`id`, `name`) VALUES
+(1, 'anees'),
+(2, 'rehman');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trig`
+--
+
+CREATE TABLE `trig` (
+  `id` int(11) NOT NULL,
+  `hcode` varchar(50) NOT NULL,
+  `normalbed` int(11) NOT NULL,
+  `hicubed` int(11) NOT NULL,
+  `icubed` int(11) NOT NULL,
+  `vbed` int(11) NOT NULL,
+  `querys` varchar(50) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `trig`
+--
+
+INSERT INTO `trig` (`id`, `hcode`, `normalbed`, `hicubed`, `icubed`, `vbed`, `querys`, `date`) VALUES
+(1, 'BBH01', 50, 9, 2, 1, ' UPDATED', '2021-11-26'),
+(2, 'BBH01', 50, 9, 2, 1, ' DELETED', '2021-11-26');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `srfid` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `dob` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `srfid`, `email`, `dob`) VALUES
+(9, 'KA20210011', 'ark@gmail.com', 'pbkdf2:sha256:260000$AhqgDCims0G1LSEi$ada839cc254cd79f9708e777ae02d83cec210677c342e01c3affd8c1358775d9'),
+(10, 'KA20210022', 'rehman@gmail.com', 'pbkdf2:sha256:260000$74GEC2qyVtOiPl5s$2a95f811bbd5a50eaac0404fb8fa3682b6c3b67f4493037134c9672393136694');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bookingpatient`
+--
+ALTER TABLE `bookingpatient`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `srfid` (`srfid`(20));
+
+--
+-- Indexes for table `hospitaldata`
+--
+ALTER TABLE `hospitaldata`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `hcode` (`hcode`);
+
+--
+-- Indexes for table `hospitaluser`
+--
+ALTER TABLE `hospitaluser`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `test`
+--
+ALTER TABLE `test`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `trig`
+--
+ALTER TABLE `trig`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `srfid` (`srfid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bookingpatient`
+--
+ALTER TABLE `bookingpatient`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `hospitaldata`
+--
+ALTER TABLE `hospitaldata`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `hospitaluser`
+--
+ALTER TABLE `hospitaluser`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `test`
+--
+ALTER TABLE `test`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `trig`
+--
+ALTER TABLE `trig`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ project/main.py
@@ -0,0 +1,374 @@
+from flask import Flask, json,redirect,render_template,flash,request
+from flask.globals import request, session
+from flask.helpers import url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash,check_password_hash
+
+from flask_login import login_required,logout_user,login_user,login_manager,LoginManager,current_user
+
+from flask_mail import Mail
+import json
+
+
+# mydatabase connection
+local_server=True
+app=Flask(__name__)
+app.secret_key="aneesrehmankhan"
+
+
+with open('config.json','r') as c:
+    params=json.load(c)["params"]
+
+
+
+app.config.update(
+    MAIL_SERVER='smtp.gmail.com',
+    MAIL_PORT='465',
+    MAIL_USE_SSL=True,
+    MAIL_USERNAME=params['gmail-user'],
+    MAIL_PASSWORD=params['gmail-password']
+)
+mail = Mail(app)
+
+
+
+# this is for getting the unique user access
+login_manager=LoginManager(app)
+login_manager.login_view='login'
+
+# app.config['SQLALCHEMY_DATABASE_URI']='mysql://username:password@localhost/databsename'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:@localhost/covid'
+db=SQLAlchemy(app)
+
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id)) or Hospitaluser.query.get(int(user_id))
+
+
+class Test(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(50))
+
+
+class User(UserMixin,db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    srfid=db.Column(db.String(20),unique=True)
+    email=db.Column(db.String(50))
+    dob=db.Column(db.String(1000))
+
+
+class Hospitaluser(UserMixin,db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    hcode=db.Column(db.String(20))
+    email=db.Column(db.String(50))
+    password=db.Column(db.String(1000))
+
+
+class Hospitaldata(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    hcode=db.Column(db.String(20),unique=True)
+    hname=db.Column(db.String(100))
+    normalbed=db.Column(db.Integer)
+    hicubed=db.Column(db.Integer)
+    icubed=db.Column(db.Integer)
+    vbed=db.Column(db.Integer)
+
+class Trig(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    hcode=db.Column(db.String(20))
+    normalbed=db.Column(db.Integer)
+    hicubed=db.Column(db.Integer)
+    icubed=db.Column(db.Integer)
+    vbed=db.Column(db.Integer)
+    querys=db.Column(db.String(50))
+    date=db.Column(db.String(50))
+
+class Bookingpatient(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    srfid=db.Column(db.String(20),unique=True)
+    bedtype=db.Column(db.String(100))
+    hcode=db.Column(db.String(20))
+    spo2=db.Column(db.Integer)
+    pname=db.Column(db.String(100))
+    pphone=db.Column(db.String(100))
+    paddress=db.Column(db.String(100))
+
+
+@app.route("/")
+def home():
+   
+    return render_template("index.html")
+
+@app.route("/trigers")
+def trigers():
+    query=Trig.query.all() 
+    return render_template("trigers.html",query=query)
+
+
+@app.route('/signup',methods=['POST','GET'])
+def signup():
+    if request.method=="POST":
+        srfid=request.form.get('srf')
+        email=request.form.get('email')
+        dob=request.form.get('dob')
+        # print(srfid,email,dob)
+        encpassword=generate_password_hash(dob)
+        user=User.query.filter_by(srfid=srfid).first()
+        emailUser=User.query.filter_by(email=email).first()
+        if user or emailUser:
+            flash("Email or srif is already taken","warning")
+            return render_template("usersignup.html")
+        new_user=db.engine.execute(f"INSERT INTO `user` (`srfid`,`email`,`dob`) VALUES ('{srfid}','{email}','{encpassword}') ")
+                
+        flash("SignUp Success Please Login","success")
+        return render_template("userlogin.html")
+
+    return render_template("usersignup.html")
+
+
+@app.route('/login',methods=['POST','GET'])
+def login():
+    if request.method=="POST":
+        srfid=request.form.get('srf')
+        dob=request.form.get('dob')
+        user=User.query.filter_by(srfid=srfid).first()
+        if user and check_password_hash(user.dob,dob):
+            login_user(user)
+            flash("Login Success","info")
+            return render_template("index.html")
+        else:
+            flash("Invalid Credentials","danger")
+            return render_template("userlogin.html")
+
+
+    return render_template("userlogin.html")
+
+@app.route('/hospitallogin',methods=['POST','GET'])
+def hospitallogin():
+    if request.method=="POST":
+        email=request.form.get('email')
+        password=request.form.get('password')
+        user=Hospitaluser.query.filter_by(email=email).first()
+        if user and check_password_hash(user.password,password):
+            login_user(user)
+            flash("Login Success","info")
+            return render_template("index.html")
+        else:
+            flash("Invalid Credentials","danger")
+            return render_template("hospitallogin.html")
+
+
+    return render_template("hospitallogin.html")
+
+@app.route('/admin',methods=['POST','GET'])
+def admin():
+ 
+    if request.method=="POST":
+        username=request.form.get('username')
+        password=request.form.get('password')
+        if(username==params['user'] and password==params['password']):
+            session['user']=username
+            flash("login success","info")
+            return render_template("addHosUser.html")
+        else:
+            flash("Invalid Credentials","danger")
+
+    return render_template("admin.html")
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("Logout SuccessFul","warning")
+    return redirect(url_for('login'))
+
+
+
+@app.route('/addHospitalUser',methods=['POST','GET'])
+def hospitalUser():
+   
+    if('user' in session and session['user']==params['user']):
+      
+        if request.method=="POST":
+            hcode=request.form.get('hcode')
+            email=request.form.get('email')
+            password=request.form.get('password')        
+            encpassword=generate_password_hash(password)  
+            hcode=hcode.upper()      
+            emailUser=Hospitaluser.query.filter_by(email=email).first()
+            if  emailUser:
+                flash("Email or srif is already taken","warning")
+         
+            db.engine.execute(f"INSERT INTO `hospitaluser` (`hcode`,`email`,`password`) VALUES ('{hcode}','{email}','{encpassword}') ")
+           
+            mail.send_message('COVID CARE CENTER',sender=params['gmail-user'],recipients=[email],body=f"Welcome thanks for choosing us\nYour Login Credentials Are:\n Email Address: {email}\nPassword: {password}\n\nHospital Code {hcode}\n\n Do not share your password\n\n\nThank You..." )
+            flash("Data Sent and Inserted Successfully","warning")
+            return render_template("addHosUser.html")
+    else:
+        flash("Login and try Again","warning")
+        return render_template("addHosUser.html")
+    
+
+
+# testing wheather db is connected or not  
+@app.route("/test")
+def test():
+    try:
+        a=Test.query.all()
+        print(a)
+        return f'MY DATABASE IS CONNECTED'
+    except Exception as e:
+        print(e)
+        return f'MY DATABASE IS NOT CONNECTED {e}'
+
+@app.route("/logoutadmin")
+def logoutadmin():
+    session.pop('user')
+    flash("You are logout admin", "primary")
+
+    return redirect('/admin')
+
+
+@app.route("/addhospitalinfo",methods=['POST','GET'])
+def addhospitalinfo():
+    email=current_user.email
+    posts=Hospitaluser.query.filter_by(email=email).first()
+    code=posts.hcode
+    postsdata=Hospitaldata.query.filter_by(hcode=code).first()
+
+    if request.method=="POST":
+        hcode=request.form.get('hcode')
+        hname=request.form.get('hname')
+        nbed=request.form.get('normalbed')
+        hbed=request.form.get('hicubeds')
+        ibed=request.form.get('icubeds')
+        vbed=request.form.get('ventbeds')
+        hcode=hcode.upper()
+        huser=Hospitaluser.query.filter_by(hcode=hcode).first()
+        hduser=Hospitaldata.query.filter_by(hcode=hcode).first()
+        if hduser:
+            flash("Data is already Present you can update it..","primary")
+            return render_template("hospitaldata.html")
+        if huser:            
+            db.engine.execute(f"INSERT INTO `hospitaldata` (`hcode`,`hname`,`normalbed`,`hicubed`,`icubed`,`vbed`) VALUES ('{hcode}','{hname}','{nbed}','{hbed}','{ibed}','{vbed}')")
+            flash("Data Is Added","primary")
+        else:
+            flash("Hospital Code not Exist","warning")
+
+
+
+
+    return render_template("hospitaldata.html",postsdata=postsdata)
+
+
+@app.route("/hedit/<string:id>",methods=['POST','GET'])
+@login_required
+def hedit(id):
+    posts=Hospitaldata.query.filter_by(id=id).first()
+  
+    if request.method=="POST":
+        hcode=request.form.get('hcode')
+        hname=request.form.get('hname')
+        nbed=request.form.get('normalbed')
+        hbed=request.form.get('hicubeds')
+        ibed=request.form.get('icubeds')
+        vbed=request.form.get('ventbeds')
+        hcode=hcode.upper()
+        db.engine.execute(f"UPDATE `hospitaldata` SET `hcode` ='{hcode}',`hname`='{hname}',`normalbed`='{nbed}',`hicubed`='{hbed}',`icubed`='{ibed}',`vbed`='{vbed}' WHERE `hospitaldata`.`id`={id}")
+        flash("Slot Updated","info")
+        return redirect("/addhospitalinfo")
+
+    # posts=Hospitaldata.query.filter_by(id=id).first()
+    return render_template("hedit.html",posts=posts)
+
+
+@app.route("/hdelete/<string:id>",methods=['POST','GET'])
+@login_required
+def hdelete(id):
+    db.engine.execute(f"DELETE FROM `hospitaldata` WHERE `hospitaldata`.`id`={id}")
+    flash("Date Deleted","danger")
+    return redirect("/addhospitalinfo")
+
+
+@app.route("/pdetails",methods=['GET'])
+@login_required
+def pdetails():
+    code=current_user.srfid
+    print(code)
+    data=Bookingpatient.query.filter_by(srfid=code).first()
+   
+    
+    return render_template("detials.html",data=data)
+
+
+@app.route("/slotbooking",methods=['POST','GET'])
+@login_required
+def slotbooking():
+    query=db.engine.execute(f"SELECT * FROM `hospitaldata` ")
+    if request.method=="POST":
+        srfid=request.form.get('srfid')
+        bedtype=request.form.get('bedtype')
+        hcode=request.form.get('hcode')
+        spo2=request.form.get('spo2')
+        pname=request.form.get('pname')
+        pphone=request.form.get('pphone')
+        paddress=request.form.get('paddress')  
+        check2=Hospitaldata.query.filter_by(hcode=hcode).first()
+        if not check2:
+            flash("Hospital Code not exist","warning")
+
+        code=hcode
+        dbb=db.engine.execute(f"SELECT * FROM `hospitaldata` WHERE `hospitaldata`.`hcode`='{code}' ")        
+        bedtype=bedtype
+        if bedtype=="NormalBed":       
+            for d in dbb:
+                seat=d.normalbed
+                print(seat)
+                ar=Hospitaldata.query.filter_by(hcode=code).first()
+                ar.normalbed=seat-1
+                db.session.commit()
+                
+            
+        elif bedtype=="HICUBed":      
+            for d in dbb:
+                seat=d.hicubed
+                print(seat)
+                ar=Hospitaldata.query.filter_by(hcode=code).first()
+                ar.hicubed=seat-1
+                db.session.commit()
+
+        elif bedtype=="ICUBed":     
+            for d in dbb:
+                seat=d.icubed
+                print(seat)
+                ar=Hospitaldata.query.filter_by(hcode=code).first()
+                ar.icubed=seat-1
+                db.session.commit()
+
+        elif bedtype=="VENTILATORBed": 
+            for d in dbb:
+                seat=d.vbed
+                ar=Hospitaldata.query.filter_by(hcode=code).first()
+                ar.vbed=seat-1
+                db.session.commit()
+        else:
+            pass
+
+        check=Hospitaldata.query.filter_by(hcode=hcode).first()
+        if(seat>0 and check):
+            res=Bookingpatient(srfid=srfid,bedtype=bedtype,hcode=hcode,spo2=spo2,pname=pname,pphone=pphone,paddress=paddress)
+            db.session.add(res)
+            db.session.commit()
+            flash("Slot is Booked kindly Visit Hospital for Further Procedure","success")
+        else:
+            flash("Something Went Wrong","danger")
+    
+    return render_template("booking.html",query=query)
+
+
+
+
+app.run(debug=True)
